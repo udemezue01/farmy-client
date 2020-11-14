f6bbfbfc40665ded1e56b12eaa096d6b9da1023a
Udemezue John <johnzeus14@gmail.com>
2020-11-14 02:09:53 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+// Tailwinds import
+
+import '@/assets/css/tailwind.css'
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
